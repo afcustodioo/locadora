229c5ad8ab5ddf93d4c7fb94fdba1a5ad7751137
Anderson Fernando Custódio <anderson.custodio@elotech.com.br>
2017-03-22 21:57:24 -0300

    Convertendo response

--- locadora-client/src/app/veiculo/veiculo.component.ts
+++ locadora-client/src/app/veiculo/veiculo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Http } from '@angular/http';
+import { Veiculo } from './veiculo.type';
 
 @Component({
   selector: 'app-veiculo-component',
@@ -11,11 +12,14 @@ export class VeiculoComponent implements OnInit {
 
   form: FormGroup;
 
+  veiculos: Veiculo[] = [];
+
   constructor(public formBuilder: FormBuilder, private http: Http) {
   }
 
   ngOnInit(): void {
     this.form = this.createForm();
+    this.loadTable();
   }
 
   save(body: any): void {
@@ -24,9 +28,32 @@ export class VeiculoComponent implements OnInit {
       delete body['id'];
     }
 
-    this.http.post('http://localhost:8080/veiculoes/', body).subscribe(salvo => {
+    this.http.post('http://localhost:8080/veiculos/', body).subscribe(salvo => {
       console.log(salvo);
     })
+  }
+
+  loadTable(): void {
+    this.http.get('http://localhost:8080/veiculos/').subscribe((data: any) => {
+
+      data.json()._embedded.veiculoes.forEach(veiculo => {
+        this.veiculos.push(this.toVeiculo(veiculo));
+      });
+
+    });
+  }
+
+  private toVeiculo(obj:any): Veiculo {
+    let v: Veiculo = <Veiculo> ({
+      ano: obj.ano,
+      modelo: obj.modelo,
+      placa: obj.placa,
+      cor: obj.cor
+    });
+
+    console.log('convertido', v);
+
+    return v;
   }
 
   private createForm(): FormGroup {
--- locadora-server/src/main/java/br/com/locadora/repository/VeiculoRepository.java
+++ locadora-server/src/main/java/br/com/locadora/repository/VeiculoRepository.java
@@ -1,11 +1,13 @@
 package br.com.locadora.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.com.locadora.model.Veiculo;
 
 @CrossOrigin
+@RepositoryRestResource(path = "veiculos")
 public interface VeiculoRepository extends PagingAndSortingRepository<Veiculo, Long> {
 
 }
